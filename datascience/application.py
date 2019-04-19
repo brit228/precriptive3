06ec3a18b8e772ceda6b7d7c4cd8cde396c52be0
Michael Beck <brit228@gmail.com>
2019-04-19 11:00:14 -0400

    Update

--- datascience/application.py
+++ datascience/application.py
@@ -2,22 +2,53 @@
 import json
 import dill
 import numpy as np
-from flask import Flask, request
+import requests
+from decouple import config
+from flask import Flask, request, Response
 from flaskrun import flaskrun
 
 
+USERNAME = os.environ["USERNAME"]
+PASSWORD = os.environ["PASSWORD"]
+BACKEND_URL = os.environ["BACKEND_URL"]
+AUTH_TOKEN = "hkdgbakuygvbuykasgvbduyk"
+
 application = Flask(__name__, static_folder=None)
 
-with open("/home/ec2-user/python-flask-service/predict.pk", "rb") as fp:
+with open("predict.pk", "rb") as fp:
     model = dill.load(fp)
 
 @application.route("/predict", methods=["POST"])
 def predict():
     smiles = request.form.get('smiles')
     pr_seq = request.form.get('sequence')
-    if smiles != "" and pr_seq != "":
-        return "{:.5f}".format(model.predict_proba(np.array([[smiles, pr_seq]]))[0,1])
-    return str(-1.0)
+    effect_id = request.form.get('effect_id')
+    d = {
+        'effect_id': effect_id,
+        'smiles': smiles,
+        'sequence': pr_seq,
+        'bind_chance': -1.0
+    }
+    status = 401
+    if smiles != "" and pr_seq != "" and effect_id != "" and effect_id:
+        try:
+            if effect_id:
+                effect_id = int(effect_id)
+            d = {
+                'effect_id': effect_id,
+                'smiles': smiles,
+                'sequence': pr_seq,
+                'bind_chance': -1.0
+            }
+            d["bind_chance"] = float(model.predict_proba(np.array([[smiles, pr_seq]]))[0,1])
+            status = 200
+        except:
+            pass
+    resp = Response(json.dumps(d), status=status, mimetype='application/json')
+    resp.headers['Authorization'] = AUTH_TOKEN
+    return resp
 
 if __name__ == '__main__':
+    r = requests.post(BACKEND_URL, data={"username": USERNAME, "password": PASSWORD})
+    AUTH_TOKEN = r.json().get('token')
     flaskrun(application)
